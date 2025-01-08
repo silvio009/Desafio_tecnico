b31a96c9f0f655e631e29eb6c5e7663a0796b910
silvio009 <rm550821@fiap.com>
2025-01-08 15:37:55 -0300

    add banco de dados

--- scicrop/blog_project/blog_project/settings.py
+++ scicrop/blog_project/blog_project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'oracle.fiap.com.br:1521/ORCL',  # Use o service name correto
+        'USER': 'rm550821',
+        'PASSWORD': 'fiap23',
     }
 }
 
